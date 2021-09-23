82044815b189f7545fcdac60de1d2f0095e965de
iamashraf21 <57479718+iamashraf21@users.noreply.github.com>
2021-09-23 11:52:15 +0530

    Update run-exe.py

--- test/run-exe.py
+++ test/run-exe.py
@@ -1,8 +1,7 @@
 import os
 import shutil
 
- 
-os.system('cd ../test')
-os.system('./test.out > output.txt')
 
+os.system('./test.out')
+os.system('./test.out > output.txt')
 os.system('python SerialRead.py')
