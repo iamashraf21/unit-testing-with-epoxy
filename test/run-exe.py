bb5010006cda1ea743aa0acf264b72ab0348656d
iamashraf21 <57479718+iamashraf21@users.noreply.github.com>
2021-09-23 12:16:10 +0530

    Update run-exe.py

--- test/run-exe.py
+++ test/run-exe.py
@@ -3,6 +3,9 @@ import shutil
 
 
 os.system('./test.out')
+os.system('ls -l')
 os.system('echo "test.out ran succes"')
 os.system('./test.out > output.txt')
+os.system('ls -l')
+os.system('cat output.txt')
 #os.system('python SerialRead.py')
