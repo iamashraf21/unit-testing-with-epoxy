76dd1a76e2a0a1dc27d89550630449f6a2691330
iamashraf21 <57479718+iamashraf21@users.noreply.github.com>
2021-09-23 16:44:18 +0530

    Update run-exe.py

--- test/run-exe.py
+++ test/run-exe.py
@@ -2,9 +2,9 @@ import os
 import shutil
 
 
-#os.system('./test.out')
+os.system('./test.out')
 #os.system('printenv')
-#os.system('echo "test.out ran succes"')
+os.system('echo "test.out ran succes"')
 #os.system('./test.out > output.txt')
 #os.system('sleep 7')
 #os.system('ls -l')
